c352c32f6e040e496fb36c2719bfd42b154d2b65
levtienbk56 <levtienbk56@gmail.com>
2021-01-03 18:40:07 +0900

    init project

---
+++ src/main/java/com/amazonaws/lambda/demo/LambdaFunctionHandler.java
@@ -0,0 +1,18 @@
+package com.amazonaws.lambda.demo;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class LambdaFunctionHandler implements RequestHandler<Object, String> {
+
+    @Override
+    public String handleRequest(Object input, Context context) {
+        LambdaLogger logger = context.getLogger();
+        logger.log("input:" + input);
+
+        // TODO: implement your handler
+        return "Hello from Lambda!";
+    }
+
+}
